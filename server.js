4a444cfa2a7c6d283f5a5de7282bb719290b670f
ADEGBITE JOSHUA ADEOYE <adegbitejoshua07@gmail.com>
2024-07-30 12:25:43 +0100

    Added the frontend url

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 app.use(cookieParser());
 app.use(cors({
-  origin: ['http://localhost:5173'],
+  origin: ['http://localhost:5173', 'https://quadrio-frontend.vercel.app'],
   credentials: true,
   allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With'],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS']
